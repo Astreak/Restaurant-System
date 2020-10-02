d4c3018b852d4b972d0cc916f180a11685c1c326
praj000 <parajbhattacharjee123@gmail.com>
2020-10-02 14:51:23 +0530

    P

--- Restraunt/Order/urls.py
+++ Restraunt/Order/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from . import views
+from .views import *
 from django.contrib.auth.views import *
 import os
 from .models import *
@@ -11,7 +12,16 @@ urlpatterns=[
     path("logout/",LogoutView.as_view(template_name="Order/logout.html")),
     path("secret/",views.employee,name="Employee"),
     path("monetary/",views.monetary,name="main-Money"),
-    path('order/',views.order,name="order")
+    path('order/',views.order,name="order"),
+    path('show/',Getting_order.as_view(),name="show-order"),
+    path('employee/',SHOW.as_view(),name="Emp"),
+    path('myorder/',MyOrder.as_view(),name="MOR"),
+    path('delivered/<int:id>',views.delivered,name="last"),
+    path('account/',views.ACCOUNT,name="money"),
+    
+    
+    
+    
     
     
     
--- Restraunt/Order/views.py
+++ Restraunt/Order/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import *
 from .forms import *
 
 def home(req):
-    return render(req,"Order/home.html");
+    J=Employee.objects.filter(account=req.user).first()
+    I=Order.objects.filter(response=J)
+    cnt=I.count()
+    return render(req,"Order/home.html",{"H":cnt});
 def info(req):
     return HttpResponse("Hey there Work on progress");
 
@@ -42,18 +47,10 @@ def order(req):
     if req.method=="POST":
         form=OrderForm(req.POST)
         J=req.POST.get("buyer")
-        
         R=req.POST.get("response")
+       
         if form.is_valid() and J!=R:
-            M={"PIZZA":123,"MACCARONI":334,"PASTA":90,"Burger":50,"Chicken":100}
-            A=req.POST.get("Choice")
-            bo=int(req.POST.get("B"))
-            k=int(M[A])
-            print(k)
-            g=Employee.objects.filter(id=R).first()
-            O=Monetary.objects.filter(user=g).first()
-            O.Money+=(k+bo)
-            O.save()
+           
             
            
             form.save()
@@ -62,3 +59,44 @@ def order(req):
         form=OrderForm()
     return render(req,"Order/Order.html",{"F":form})
 
+class Getting_order(ListView):
+    template_name="Order/show.html"
+    def get_queryset(self):
+        
+        J=Employee.objects.filter(account=self.request.user).first()
+        Q=Order.objects.filter(response=J)
+        return Q;
+
+
+class SHOW(ListView):
+    template_name="Order/employee.html"
+    def get_queryset(self):
+        Q=Employee.objects.all()
+        return Q;
+
+class MyOrder(ListView):
+    template_name="Order/MyOrder.html"
+    def get_queryset(self):
+        Q=Order.objects.filter(buyer=self.request.user)
+        return Q;
+
+def delivered(req,id):
+    M={"PIZZA":123,"MACCARONI":334,"PASTA":90,"Burger":50,"Chicken":100}
+    V=Order.objects.filter(id=id).first()
+    R=V.response
+    A=V.Choice
+    bo=int(V.B)
+    k=int(M[A])
+   
+    g=Employee.objects.filter(id=R.id).first()
+    O=Monetary.objects.filter(user=g).first()
+    O.Money+=k
+    O.bonus=bo;
+    O.save()
+    Order.objects.filter(id=id).delete()
+    return redirect('main-home')
+def ACCOUNT(req):
+    A=Employee.objects.filter(account=req.user).first()
+    print(A)
+    G=Monetary.objects.filter(user=A).first()
+    return render(req,"Order/account.html",{"F":G})
